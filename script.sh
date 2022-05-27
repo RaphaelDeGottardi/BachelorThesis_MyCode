a2790d017b9e8c02323112dbfc206b2dfaecde8c
Raphael <raphael.degottardi@outlook.com>
2022-05-27 18:51:07 +0200

    worked on script file for ubelix

--- script.sh
+++ script.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
-SBATCH --mail-user= raphael.degottardi@students.unibe.ch
-SBATCH --mail-type=end,fail
+#SBATCH --mail-user= raphael.degottardi@students.unibe.ch
+#SBATCH --mail-type=end,fail
 
-SBATCH --mem-per-cpu=16G
-SBATCH --cpus-per-task=6
-SBATCH --time=0-01:00:00
-SBATCH --output=./slurms/test_with_preds_%A_%a.out
-SBATCH --array=0
+#SBATCH --job-name="GED with WL-hashes"
+#SBATCH --mem-per-cpu=16G
+#SBATCH --cpus-per-task=6
+#SBATCH --time=0-01:00:00
+#SBATCH --output=./slurms/test_with_preds_%A_%a.out
+#SBATCH --array=1
 
 
-#param_store=$HOME/args.txt
+param_store=$HOME/Info_Bachelor/BachelorThesis_MyCode/args.txt
 
 # Get first argument
-#dataset=$(cat $param_store | awk -v var=$SLURM_ARRAY_TASK_ID 'NR==var {print $1}')
+dataset=$(cat $param_store | awk -v var=$SLURM_ARRAY_TASK_ID 'NR==var {print $1}')
 
 # Put your code below this line
-#module load Python/3.9
-#cd $HOME/graph_matching/graph-matching-gnn-reduction
-#source venv/bin/activate
+module load Python/3.9
+cd $HOMEE/Info_Bachelor/graph_matching-core
+source venv/bin/activate
+echo "I'm on host: $HOSTNAME"
 
-srun python Protoype_script.py #--dataset $dataset
+cd $HOMEE/Info_Bachelor/BachelorThesis_MyCode
+srun python Protoype_script.py --dataset $dataset
 
