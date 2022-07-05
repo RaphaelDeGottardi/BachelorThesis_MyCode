9423acaba815f45e0e7cbb6532b2cf918080d25f
Raphael <raphael.degottardi@outlook.com>
2022-07-05 15:21:11 +0200

    test file to test ubelix

---
+++ test_script.py
@@ -0,0 +1,20 @@
+from itertools import combinations
+from time import time
+
+import os
+import numpy as np
+import networkx as nx
+import shutil
+import itertools
+
+#prototype to test the kNN classification aughmented with the WL-hashes
+
+from graph_pkg_core.coordinator.coordinator_vector_classifier import CoordinatorVectorClassifier
+from graph_pkg_core.algorithm.knn import KNNClassifier
+from graph_pkg_core.utils.functions.helper import calc_accuracy
+
+
+def main():
+    print("this works")
+if __name__ == "__main__":
+    main()
