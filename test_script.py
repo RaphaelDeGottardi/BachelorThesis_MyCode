702f4eaa6817c585a75fc998fc03096f8f8a5c63
Raphael <raphael.degottardi@outlook.com>
2022-07-07 09:52:48 +0200

    a

--- test_script.py
+++ test_script.py
@@ -30,7 +30,7 @@ def main():
 
     graph_list = []
 
-    loader_vector = LoaderVector(f'./MyCode/WL_graphs_enzymes', use_wl_attr = True)    
+    loader_vector = LoaderVector(f'./WL_graphs_enzymes', use_wl_attr = True)    
     WL_graph_list = loader_vector.load()
 
 
