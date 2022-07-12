2b8b6667efc738f3c62ee4ef6f8d2b82d7f1d198
Raphael <raphael.degottardi@outlook.com>
2022-07-12 13:23:34 +0200

    experiments with different weights

--- Experiment_mean_enzy.py
+++ Experiment_mean_enzy.py
@@ -17,45 +17,54 @@ from graph_pkg_core.utils.functions.helper import calc_accuracy
 def main():
    #Loading the Graphs
     shufflelist = [1651,21465,27709,44332,53628,54292,56261,60278,77558,80822]
-    for kNN in [1,3]:
+    weights = [[1/8,0,0],[0,0,0.2],[0.1,0.1,0.1],[0.125,0,0],[0,0,0]]
+    for weight in weights:
+        for kNN in [1,3]:
+            accuracies = []
 
-        for shuffle in shufflelist:
-            print(f'Experiment to calculate the mean accuracy(k={kNN}) of the prediction on the original graphs, seed:{shuffle}')
-    
-            shuffle_str = str(shuffle)
-            FOLDER_DATA = os.path.join(os.path.dirname(__file__),
-                                f'WL_graphs_enzymes/{shuffle}')
+            for shuffle in shufflelist:
+                print(f'Experiment to calculate the mean accuracy(k={kNN}) of the prediction on the original graphs, seed:{shuffle}')
+        
+                shuffle_str = str(shuffle)
+                FOLDER_DATA = os.path.join(os.path.dirname(__file__),
+                                    f'WL_graphs_enzymes/{shuffle}')
 
-            wl_k = 3 #for WL algorithm
+                wl_k = 3 #for WL algorithm
 
 
-            coordinator = CoordinatorVectorClassifier('enzymes',
-                                            (1., 1., 1., 1., 'euclidean',wl_k,[0,0,0]),
-                                            FOLDER_DATA,None,True,False)
+                coordinator = CoordinatorVectorClassifier('enzymes',
+                                                (1., 1., 1., 1., 'euclidean',wl_k,[weight[0],weight[1],weight[2]]),
+                                                FOLDER_DATA,None,True,False)
 
-            X_train, y_train = getattr(coordinator, 'train_split')()
-            X_test, y_test = getattr(coordinator, 'test_split')()
-            X_validation, y_validation = getattr(coordinator, 'val_split')()
+                X_train, y_train = getattr(coordinator, 'train_split')()
+                X_test, y_test = getattr(coordinator, 'test_split')()
+                X_validation, y_validation = getattr(coordinator, 'val_split')()
 
-            knn = KNNClassifier(coordinator.ged, True, verbose=False)
-            knn.train(graphs_train=X_train, labels_train=y_train)
+                knn = KNNClassifier(coordinator.ged, True, verbose=False)
+                knn.train(graphs_train=X_train, labels_train=y_train)
 
-            start_time = time()
-            predictions = knn.predict(X_validation, k=1, num_cores=8)
-            prediction_time = time() - start_time
-            acc = calc_accuracy(np.array(y_validation, dtype=np.int32),
-                                np.array(predictions, dtype=np.int32))    
-            message = f'Best acc on val: {acc:.2f}, time: {prediction_time:.2f}s\n'
-            print(message)  
+                start_time = time()
+                predictions = knn.predict(X_validation, k=1, num_cores=8)
+                prediction_time = time() - start_time
+                acc = calc_accuracy(np.array(y_validation, dtype=np.int32),
+                                    np.array(predictions, dtype=np.int32))    
+                accuracies.append(acc)
+                message = f'Best acc on val: {acc:.2f}, time: {prediction_time:.2f}s\n'
+                print(message)  
 
-            start_time = time()
-            predictions = knn.predict(X_test, k=1, num_cores=8)
-            prediction_time = time() - start_time
-            acc = calc_accuracy(np.array(y_test, dtype=np.int32),
-                                np.array(predictions, dtype=np.int32))
-
-            message = f'Best acc on test: {acc:.2f}, time: {prediction_time:.2f}s\n'
-            print(message)
+                start_time = time()
+                predictions = knn.predict(X_test, k=1, num_cores=8)
+                prediction_time = time() - start_time
+                acc = calc_accuracy(np.array(y_test, dtype=np.int32),
+                                    np.array(predictions, dtype=np.int32))
+                accuracies.append(acc)
+                message = f'Best acc on test: {acc:.2f}, time: {prediction_time:.2f}s\n'
+                print(message)
+            print(f'For the enzymes daaset, k = {kNN} and the weights: {weight} the mean is:')
+            np.mean(accuracies)
+            print('and the std is:')
+            np.std(accuracies)
+            print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
 
 if __name__ == "__main__":
     main()
